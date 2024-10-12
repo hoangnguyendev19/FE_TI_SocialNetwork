1b10465832e6338ade8c51dc93a797b8df32485f
hoangnguyendev19 <hoangngocnguyen2021@gmail.com>
2024-10-12 14:59:43 +0700

    Refactor code formatting settings and add Google Maps API dependencies

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,37 @@
-import { Typography } from "antd";
-import { Color } from "constants";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import React from "react";
 
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat: 13.78, // Latitude of Bình Định
+  lng: 109.2198, // Longitude of Bình Định
+};
+
 export const HomePage: React.FC = () => {
-  return (
-    <div>
-      <Typography.Title level={1} style={{ color: Color.SENCONDARY }}>
-        Home page!
-      </Typography.Title>
-    </div>
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
+  });
+
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+
+  const onLoad = React.useCallback((map: google.maps.Map) => {
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(() => {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+      {/* Add any child components like markers here */}
+    </GoogleMap>
+  ) : (
+    <div>Loading...</div>
   );
 };
