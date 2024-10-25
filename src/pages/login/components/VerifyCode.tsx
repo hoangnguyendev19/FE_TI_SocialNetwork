73db806ef3aa21b5c6b9e94ee002ee007b0877fa
hoangnguyendev19 <hoangngocnguyen2021@gmail.com>
2024-10-25 09:56:42 +0700

    Refactor ForgotPassword, VerifyCode, SetPassword components for improved code readability

--- src/pages/login/components/VerifyCode.tsx
+++ src/pages/login/components/VerifyCode.tsx
@@ -1,24 +1,15 @@
 import { LeftOutlined } from "@ant-design/icons";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useMutation } from "@tanstack/react-query";
-import {
-  Button,
-  Col,
-  Flex,
-  Image,
-  Input,
-  notification,
-  Row,
-  Typography,
-} from "antd";
+import { Button, Col, Flex, Image, Input, notification, Row, Typography } from "antd";
 import { authApi } from "api";
 import LoginImage from "assets/images/img-login.png";
 import { ErrorCode, ErrorMessage, ROUTE, VerifyCodeRequest } from "constants";
 import React from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
 import { imageStyle, inputErrorStyle, inputStyle } from "styles";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 export const VerifyCode: React.FC = () => {
   const navigate = useNavigate();
@@ -93,49 +84,41 @@ export const VerifyCode: React.FC = () => {
   return (
     <Row>
       <Col span={14}>
-        <Flex
-          justify="center"
-          vertical
-          style={{ height: "100%", width: "100%" }}
-        >
+        <Flex justify="center" vertical style={{ height: "100%", width: "100%" }}>
           <Typography.Link href={ROUTE.LOGIN}>
             <LeftOutlined /> Back to login
           </Typography.Link>
           <Typography.Title level={2}>Verify code</Typography.Title>
-          <Typography.Paragraph
-            type="secondary"
-            style={{ marginBottom: "15px" }}
-          >
+          <Typography.Paragraph type="secondary" style={{ marginBottom: "15px" }}>
             An authentication code has been sent to your email.
           </Typography.Paragraph>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Typography.Title level={5}>OTP</Typography.Title>
+            <Typography.Title level={5}>
+              OTP<span style={{ color: "red" }}>*</span>
+            </Typography.Title>
             <Controller
               name="otp"
               control={control}
               render={({ field }) => (
                 <Input
                   placeholder="Please type your otp!"
-                  style={inputStyle}
+                  style={{ ...inputStyle, borderColor: errors.otp ? "red" : "" }}
                   {...field}
                   aria-invalid={!!errors.otp}
                 />
               )}
             />
             <div style={inputErrorStyle}>
-              {errors.otp && (
-                <Typography.Text type="danger">
-                  {errors.otp.message}
-                </Typography.Text>
-              )}
+              {errors.otp && <Typography.Text type="danger">{errors.otp.message}</Typography.Text>}
             </div>
-
-            <Typography.Text style={{ marginTop: "10px" }}>
-              Didn’t receive a code?{" "}
-              <Typography.Link type="danger" onClick={handResend}>
-                Resend
-              </Typography.Link>
-            </Typography.Text>
+            <div style={{ marginTop: "15px" }}>
+              <Typography.Text>
+                Didn’t receive a code?{" "}
+                <Typography.Link type="danger" onClick={handResend}>
+                  Resend
+                </Typography.Link>
+              </Typography.Text>
+            </div>
 
             <Button
               type="primary"
@@ -154,12 +137,7 @@ export const VerifyCode: React.FC = () => {
       </Col>
       <Col span={10}>
         <Flex align="center" justify="end">
-          <Image
-            style={imageStyle}
-            src={LoginImage}
-            placeholder="Verify Code"
-            preview={false}
-          />
+          <Image style={imageStyle} src={LoginImage} placeholder="Verify Code" preview={false} />
         </Flex>
       </Col>
     </Row>
